d6b490ef8ea84bf861d1e08e2adf8d2e4e7618a3
zhby99 <bzhang70@illinois.edu>
2017-05-26 10:08:34 +0800

    S&P500

--- extract_data.py
+++ extract_data.py
@@ -1,2 +1,10 @@
-from yahoo_finance import Share
-yahoo = Share('YHOO')
+import pandas
+google_data = pandas.read_csv("data/GOOGL.csv")
+row_num = 0
+# for index, row in google_data.iterrows():
+#     row_num+=1
+
+data_size = google_data["Date"].count()
+
+for i in range(data_size):
+    
